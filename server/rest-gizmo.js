55840efc604d42f84655644ef811d0a696578c4f
sebastian <sebastianmac@Sebastians-MBP.home>
2015-01-13 16:45:38 -0500

    added rest service

--- server/rest-gizmo.js
+++ server/rest-gizmo.js
@@ -10,10 +10,32 @@
 
     var router = express.Router(); 
 
-    router.get('/', function (req, res) {
-        res.send('gizmos');
+    router.get('/test', function (req, res) {
+        res.send('testing gizmos');
+        
         service.sayHello();
 
+        console.log(service.getAll());
+
+        service.add({ id: 111, description: "its 111" });
+        service.add({ id: 112, description: "its 112" });
+        service.add({ id: 113, description: "its 113" });
+
+        console.log(service.getAll());
+
+        console.log(service.get(111));
+
+        service.remove(112);
+
+        console.log(service.getAll());
+
+        service.update({ id: 113, description: "its 113 tt" });
+
+        console.log(service.getAll());
+    });
+
+    router.get('/', function (req, res) {
+        res.send('gizmos');
 
     });
 
--- server/service-gizmo.js
+++ server/service-gizmo.js
@@ -2,8 +2,46 @@
 
   'use strict';
 
+  var gizmos = [];
+
   exports.sayHello = function() {
-    console.log("say hello");
+    console.log("hello from gizmo");
+  }
+
+  exports.add = function(gizmo) {
+    gizmos.push(gizmo);
+  }
+
+  exports.getAll = function() {
+    return gizmos;
+  }
+
+  exports.get = function(gid) {
+    for (var i = 0; i < gizmos.length; i++) {
+        if (gizmos[i].id === gid) {
+          return gizmos[i];
+        }
+    }
+    return null;
+  }
+
+  exports.remove = function(gid) {
+    var index = findIndex(gid);
+    gizmos.splice(index,1);
+  }
+
+  exports.update = function(gizmo) {
+    var index = findIndex(gizmo.id);
+    gizmos.splice(index,1,gizmo);
+  }
+
+  var findIndex = function(gid) {
+    for (var i = 0; i < gizmos.length; i++) {
+        if (gizmos[i].id === gid) {
+          return i;
+        }
+    }
+    return -1;
   }
 
 })();
