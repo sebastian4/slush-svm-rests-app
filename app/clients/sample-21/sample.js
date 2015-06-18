ef05b1354e07f730bfbee7b28ef20b1466d4eab5
sebastian <sebastian.lopez.xx@gmail.com>
2015-06-17 21:57:36 -0400

    sample to test chrome dev tools

--- app/clients/sample-21/sample.js
+++ app/clients/sample-21/sample.js
@@ -2,27 +2,15 @@
 
 	'use strict';
 
-	console.log("sample 21");
+	console.log("sample 21 1");
 
 	$("#btn1").click(function() {
-		console.log("log clicked btn1");
+		//console.log("log clicked btn1");
 		console.info("btn1 info clicked");
 	});
 
 	$("#btn2").click(function() {
 		console.info("btn2 info clicked");
-	});
-
-
-	$("#btn3").click(function() {
-		console.info("btn3 info clicked");
-		// if(window.console && console.log){
-	 //        var old = console.log;
-	 //        console.log = function(){
-	 //            Array.prototype.unshift.call(arguments, 'Report: ');
-	 //            old.apply(this, arguments)
-	 //        }
-	 //    } 
 	});
 
 })();
---
+++ app/clients/sample-21/sample2.js
@@ -0,0 +1,21 @@
+(function(){
+
+	'use strict';
+
+	console.log("sample 21 2");
+
+	$("#btn3").click(function() {
+		console.info("btn3 s2 info click!");
+	});
+
+	// $("#btn3").click(function() {
+		// if(window.console && console.log){
+	 //        var old = console.log;
+	 //        console.log = function(){
+	 //            Array.prototype.unshift.call(arguments, 'Report: ');
+	 //            old.apply(this, arguments)
+	 //        }
+	 //    } 
+	// });
+
+})();
